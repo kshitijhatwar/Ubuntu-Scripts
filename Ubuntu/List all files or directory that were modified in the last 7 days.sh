9f40b2e89e2616aae64e39b84540d02e77196c49
kshitijhatwar <kshhat66885@gmail.com>
2025-04-27 17:30:51 +0530

    adding few more commands

---
+++ Ubuntu/List all files or directory that were modified in the last 7 days.sh
@@ -0,0 +1,6 @@
+#List all files that were modified in the last 7 days.
+find /path/to/search -type f -mtime -7
+
+
+#List all directory that were modified in the last 7 days.
+find /path/to/search -type f -mtime -7
