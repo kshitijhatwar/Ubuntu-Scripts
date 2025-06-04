208fe3325b6168c25a7d0382ba060ba46cfc1c94
kshitijhatwar <kshhat66885@gmail.com>
2025-06-04 10:28:15 +0530

    aded terraform

---
+++ Terraform/project1/AMI_ID.tf
@@ -0,0 +1,20 @@
+data "aws_ami" "T3_ami_ID" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+output "Instance_id" {
+  description = "AMI ID of T3 ubuntu instance"
+  value       = data.aws_ami.T3_ami_ID.id
+}
