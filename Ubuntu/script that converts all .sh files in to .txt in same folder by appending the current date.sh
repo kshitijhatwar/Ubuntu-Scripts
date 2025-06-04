a5ad7e078fcc556d78b6b5c09336843e367b340e
kshitijhatwar <kshhat66885@gmail.com>
2025-06-04 09:39:54 +0530

    update

--- Ubuntu/script that converts all .sh files in to .txt in same folder by appending the current date.sh
+++ Ubuntu/script that converts all .sh files in to .txt in same folder by appending the current date.sh
@@ -3,9 +3,11 @@ DATE=$(date +%F)
 TD="/home/"
 
 for FILE in "$TD"*.sh; do
-
+        
+        #check if there is.sh file
         [ -e "$FILE" ] || continue
 
+        # extracting basename removing all /
         BN=$(basename "$FILE" .sh)
 
         mv "$FILE" "${TD}${BN}_${DATE}.txt"
