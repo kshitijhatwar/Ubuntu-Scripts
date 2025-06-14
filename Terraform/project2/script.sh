c2cc9b1ff3cfd882bcaea4efa670f5c94736313c
kshitijhatwar <kshhat6685@gmail.com>
2025-06-14 18:08:22 +0530

    updates

--- Terraform/project2/script.sh
+++ Terraform/project2/script.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 apt update
 apt upgrade -y
+apt install unzip wget apache2 -y
+systemctl start apache2
+systemctl enable apache2
+wget -o /var/tmp/2117_infinite_loop.zip https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
+cp -r /var/tmp/2117_infinite_loop/* /var/www/html/
+systemctl restart apache2
+
